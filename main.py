d9a826391df6f73aa0b37b7cb41ac7ea4f0f7a21
Chintan <cap5192@gmail.com>
2022-06-19 21:38:23 -0500

    test

--- main.py
+++ main.py
@@ -29,7 +29,43 @@ import pandas
 
 
 # print(data[data["day"] == "Monday"])
+#count each color of squirrl and create new dataframe
+# data = pandas.read_csv("CentralPark.csv")
+# new_data = data["Primary Fur Color"] == "Gray"
+# print(data.count())
+
+# data = pandas.read_csv("weather_data.csv")
+#
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# print(pandas.DataFrame(data_dict))
 
 data = pandas.read_csv("CentralPark.csv")
-new_data = data["Primary Fur Color"] == "Gray"
-print(data.count())
+color = data["Primary Fur Color"]
+
+gray = 0
+red = 0
+black = 0
+
+for i in color:
+    if i == "Gray":
+        gray += 1
+    if i == "Black":
+        black += 1
+    if i == "Cinnamon":
+        red += 1
+
+dict = {}
+dict["Fur Color"] = ["grey", "red", "black"]
+dict["Count"] = [gray, black, red]
+
+df = pandas.DataFrame(dict)
+df.to_csv("squirrel_count.csv")
+
+
